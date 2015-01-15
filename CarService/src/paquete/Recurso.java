8fc1d0b23b38db947dccd1697b08fffbc09695aa
Luis B. <luis_ness@hotmail.com>
2015-01-15 13:05:22 +0100

    [P3] Mostrar coches HTML.
    Ya está lista la vista de mostrar coches con HTML.
    Hay un recurso que se encarga de mostrar el inventario en una tabla con
    el CSS ajustado y todo. (Esta funcionalidad es solo para ver)

--- CarService/src/paquete/Recurso.java
+++ CarService/src/paquete/Recurso.java
@@ -80,11 +80,19 @@ public class Recurso {
 	
 	/**
 	 * Este método se encarga de crear un nuevo coche.
-	 * @param id
-	 * @param nombre
-	 * @param apellidos
-	 * @param servletResponse
-	 * @throws IOException
+	 * @param precio : Precio del coche.
+	 * @param cilindros : Número de cilindros que tiene el coche.
+	 * @param caballos : Caballos del coche.
+	 * @param tamMotor : Tamaño del motor del coche.
+	 * @param peso : Peso del coche en kg.
+	 * @param longitud : Longitud del coche en m.
+	 * @param marca : Marca del coche.
+	 * @param modelo : Modelo del coche.
+	 * @param tipo : Tipo de coche 'turismo', ...
+	 * @param origen : Origen del coche.
+	 * @param traccion : Tracción del coche 'delantera', ...
+	 * @param servletResponse : Servlet que nos va a proporcionar la respesta.
+	 * @throws IOException : Lanza una excepción si existe algún problema de entrada/salida de datos.
 	 */
 	@POST
 	@Produces(MediaType.TEXT_HTML)
@@ -113,5 +121,77 @@ public class Recurso {
 		CocheDB.insertar(coche);
 	}
 	
+	/**
+	 * 
+	 * @return Devuelve el inventario de coches para mostrarlo con HTML.
+	 */
+	@GET
+	@Path("/getInventario")
+	@Produces({MediaType.TEXT_HTML})
+	public String getInventarioHTML() {
+		String inventarioHTML = "";
+		List<Coche> lc;
+		
+		// Obtenemos la lista de los coches.
+		lc = CocheDB.getCoches();
+		
+		// Cabecera del HTML.
+		inventarioHTML = "<!DOCTYPE html>"
+				+ "<html><head><meta charset=\"UTF-8\">"
+				+ "<style type=\"text/css\">"					// Estilo de la página.
+				+ ".EstiloTabla {     margin:0px;padding:0px;     width:100%;     box-shadow: 10px 10px 5px #888888;     border:1px solid #000000;          -moz-border-radius-bottomleft:0px;     -webkit-border-bottom-left-radius:0px;     border-bottom-left-radius:0px;          -moz-border-radius-bottomright:0px;     -webkit-border-bottom-right-radius:0px;     border-bottom-right-radius:0px;          -moz-border-radius-topright:0px;     -webkit-border-top-right-radius:0px;     border-top-right-radius:0px;          -moz-border-radius-topleft:0px;     -webkit-border-top-left-radius:0px;     border-top-left-radius:0px; }.EstiloTabla table{     border-collapse: collapse;         border-spacing: 0;     width:100%;     height:100%;     margin:0px;padding:0px; }.EstiloTabla tr:last-child td:last-child {     -moz-border-radius-bottomright:0px;     -webkit-border-bottom-right-radius:0px;     border-bottom-right-radius:0px; } .EstiloTabla table tr:first-child td:first-child {     -moz-border-radius-topleft:0px;     -webkit-border-top-left-radius:0px;     border-top-left-radius:0px; } .EstiloTabla table tr:first-child td:last-child {     -moz-border-radius-topright:0px;     -webkit-border-top-right-radius:0px;     border-top-right-radius:0px; }.EstiloTabla tr:last-child td:first-child{     -moz-border-radius-bottomleft:0px;     -webkit-border-bottom-left-radius:0px;     border-bottom-left-radius:0px; }.EstiloTabla tr:hover td{      } .EstiloTabla tr:nth-child(odd){ background-color:#aaffff; } .EstiloTabla tr:nth-child(even)    { background-color:#ffffff; }.EstiloTabla td{     vertical-align:middle;               border:1px solid #000000;     border-width:0px 1px 1px 0px;     text-align:left;     padding:7px;     font-size:10px;     font-family:Arial;     font-weight:normal;     color:#000000; }.EstiloTabla tr:last-child td{     border-width:0px 1px 0px 0px; }.EstiloTabla tr td:last-child{     border-width:0px 0px 1px 0px; }.EstiloTabla tr:last-child td:last-child{     border-width:0px 0px 0px 0px; } .EstiloTabla tr:first-child td{         background:-o-linear-gradient(bottom, #00ffff 5%, #0096bc 100%);    background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #00ffff), color-stop(1, #0096bc) );     background:-moz-linear-gradient( center top, #00ffff 5%, #0096bc 100% );     filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#00ffff\", endColorstr=\"#0096bc\");  background: -o-linear-gradient(top,#00ffff,0096bc);      background-color:#00ffff;     border:0px solid #000000;     text-align:center;     border-width:0px 0px 1px 1px;     font-size:14px;     font-family:Arial;     font-weight:bold;     color:#ffffff; } .EstiloTabla tr:first-child:hover td{     background:-o-linear-gradient(bottom, #00ffff 5%, #0096bc 100%);    background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #00ffff), color-stop(1, #0096bc) );     background:-moz-linear-gradient( center top, #00ffff 5%, #0096bc 100% );     filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#00ffff\", endColorstr=\"#0096bc\");  background: -o-linear-gradient(top,#00ffff,0096bc);      background-color:#00ffff; } .EstiloTabla tr:first-child td:first-child{     border-width:0px 0px 1px 0px; } .EstiloTabla tr:first-child td:last-child{     border-width:0px 0px 1px 1px; }"
+				+ "</style>"
+				+ "<div class=\"EstiloTabla\" >"
+				+ "   <table>";
+		// Ahora insertamos los elementos de la tabla.
+		// Insetamos la primera fila que tiene cada uno de los nombres de los atributos.
+		inventarioHTML = inventarioHTML + "<tr>";
+		inventarioHTML = inventarioHTML
+				+ "<td>Id </td>        "
+				+ "<td>Marca </td>      "
+		        + "<td>Modelo </td>     "
+		        + "<td>Tipo </td>       "
+		        + "<td>Origen </td>     "
+		        + "<td>Tracción </td>   "
+				+ "<td>Precio </td>    "
+		        + "<td>Cilindros </td> "
+		        + "<td>Caballos </td>  "
+		        + "<td>Tamaño motor </td>  "
+		        + "<td>Peso </td>      "
+		        + "<td>Longitud </td>  ";
+		inventarioHTML = inventarioHTML + "</tr>";
+		
+		// Ahora insertamos cada uno de los coches que hay.
+		for(Coche c : lc) {
+			// Inicio de linea.
+			inventarioHTML = inventarioHTML + "<tr>";
+			// Cada uno de los atributos.
+			inventarioHTML = inventarioHTML
+					+ "<td>" + c.getCarId() 	+ 	"</td>"
+					+ "<td>" + c.getMarca() 	+   "</td>"
+			        + "<td>" + c.getModelo() 	+   "</td>"
+			        + "<td>" + c.getTipo() 		+   "</td>"
+			        + "<td>" + c.getOrigen() 	+   "</td>"
+			        + "<td>" + c.getTraccion() 	+   "</td>"
+					+ "<td>" + Integer.toString(c.getPrecio() 	) +   "</td>"
+			        + "<td>" + Integer.toString(c.getCilindros()) +   "</td>"
+			        + "<td>" + Integer.toString(c.getCaballos()	) +    "</td>"
+			        + "<td>" + Double.toString(c.getTamMotor()	) +    "</td>"
+			        + "<td>" + Double.toString(c.getPeso()		) +    "</td>"
+			        + "<td>" + Double.toString(c.getLongitud() )  +    "</td>" ;
+			// Final de línea.
+			inventarioHTML = inventarioHTML + "</tr>";
+		}
+		
+		// Cierre de la cabecera.
+		inventarioHTML = inventarioHTML 
+				+ "   </table>"
+				+ "</div>"
+				+ "<title>Insert title here</title>"
+				+ "</head>"
+				+ "<body></body></html>";
+		
+		return inventarioHTML;
+	}
 
 }
